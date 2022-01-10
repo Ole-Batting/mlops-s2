a5c427388a0b63f630735920998b177e86d8eb38
Ole-Batting <batting.ole@gmail.com>
2022-01-10 11:10:25 +0100

    Update test_training.py

--- tests/test_training.py
+++ tests/test_training.py
@@ -2,4 +2,10 @@ import sys
 sys.path.append('src/models')
 from train_model import _main
 
-_main('data/processed', 'models/model.pth', 1, True)
+try:
+    _main('data/processed', 'models/model.pth', 1, True)
+    val = True
+except:
+    val = False
+
+assert val, 'training failed on 1 epoch.'
