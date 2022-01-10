544bdb969763add37ec2650948fd6ffca6d5ccfe
Ole-Batting <batting.ole@gmail.com>
2022-01-10 16:10:07 +0100

    Update test_training.py

--- tests/test_training.py
+++ tests/test_training.py
@@ -7,10 +7,9 @@ import torch
 x = torch.Tensor(np.ones((10,1,28,28)))
 y = torch.tensor(np.arange((10)),dtype=torch.long)
 
+val = False
 try:
     _main([[x, y]], [x, y], 'models/model.pth', 1, True)
     val = True
-except:
-    val = False
 
 assert val, 'training failed on 1 epoch.'
